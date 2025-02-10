e43708ba56b616e850291789536aebfd1094fe6c
PEDRO HENRIQUE SERRA TASSI <52820@tjmt.jus.br>
2025-02-10 14:51:11 -0400

    chat

--- chatbot/views.py
+++ chatbot/views.py
@@ -18,12 +18,14 @@ logger = logging.getLogger(__name__)
 
 # Conectar ao banco de dados PostgreSQL
 try:
-    db_uri = f"postgresql://{config('DB_USER')}:{config('DB_PASSWORD')}@{config('DB_HOST')}:{config('DB_PORT')}/{config('DB_NAME')}"
+    db_uri = f"postgresql://{config('DB_USER')}:{config('DB_PASSWORD')}@localhost:{config('DB_PORT')}/{config('DB_NAME')}"
+    print(f"Tentando conectar ao banco: {db_uri}")  # DEBUG
+
     db = SQLDatabase.from_uri(db_uri)
     logger.info("Conexão ao banco de dados PostgreSQL bem-sucedida!")
 except Exception as e:
     logger.error(f"Erro ao conectar ao banco de dados PostgreSQL: {e}")
-    db = None  # Garante que a variável 'db' existe, mesmo que a conexão falhe
+    db = None 
 
 # Verifica se o banco foi conectado antes de criar o agente SQL
 if db:
