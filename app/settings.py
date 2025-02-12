675756b5df8ae16b791e55323a4d753c6ee05a1f
PEDRO HENRIQUE SERRA TASSI <52820@tjmt.jus.br>
2025-02-12 17:23:03 -0400

    chat

--- app/settings.py
+++ app/settings.py
@@ -89,11 +89,14 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'gabinete',
+        'USER': 'postgres',
+        'PASSWORD': '03010315SSp@@#',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 
 
 
