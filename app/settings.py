0f52211d4e0c805a9e5f0bcc602993a97a5b5d3a
EduardoRezendee <e.rezendevarandas@icloud.com>
2025-05-13 13:43:48 -0400

    gpts e cor

--- app/settings.py
+++ app/settings.py
@@ -90,7 +90,6 @@ X_FRAME_OPTIONS = "SAMEORIGIN"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
