75dac677f2a9802fd560e925d28b1f28786ecccb
EduardoRezendee <e.rezendevarandas@icloud.com>
2025-06-02 20:07:27 -0400

    data

--- app/settings.py
+++ app/settings.py
@@ -94,23 +94,23 @@ X_FRAME_OPTIONS = "SAMEORIGIN"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 
-#DATABASES = {
-  #  'default': {
- #   'ENGINE': 'django.db.backends.postgresql',
- #   'NAME': 'gabjonesgattass',
-  #  'USER': 'postgres',
-  #  'PASSWORD': 'G@b202025',
- #   'HOST': 'localhost',
- #   'PORT': '5432',
-#}
-#}
-
 DATABASES = {
     'default': {
-    'ENGINE': 'django.db.backends.sqlite3',
-    'NAME': BASE_DIR / 'db.sqlite3',
+    'ENGINE': 'django.db.backends.postgresql',
+    'NAME': 'gabjonesgattass',
+    'USER': 'postgres',
+    'PASSWORD': 'G@b202025',
+    'HOST': 'localhost',
+    'PORT': '5432',
 }
 }
+
+#DATABASES = {
+ #   'default': {
+  #  'ENGINE': 'django.db.backends.sqlite3',
+   # 'NAME': BASE_DIR / 'db.sqlite3',
+#}
+#}
 
 
 # Password validation
