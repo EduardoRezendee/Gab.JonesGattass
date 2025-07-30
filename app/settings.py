e5ea8edfaa74018acd6ffc3edb043ec207fb155b
EduardoRezendee <e.rezendevarandas@icloud.com>
2025-07-30 15:07:52 -0400

    urgent correto

--- app/settings.py
+++ app/settings.py
@@ -104,9 +104,9 @@ DATABASES = {
 }
 
 #DATABASES = {
- #  'default': {
+ # 'default': {
  #'ENGINE': 'django.db.backends.sqlite3',
- #'NAME': BASE_DIR / 'db.sqlite3',
+#'NAME': BASE_DIR / 'db.sqlite3',
 #}
 #}
 
