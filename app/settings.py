e374da17463191854bb533a66b046da8365cd9e2
EduardoRezendee <e.rezendevarandas@icloud.com>
2025-11-05 18:00:03 -0400

    gorro

--- app/settings.py
+++ app/settings.py
@@ -92,23 +92,23 @@ X_FRAME_OPTIONS = "SAMEORIGIN"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 
-DATABASES = {
-'default': {
-'ENGINE': 'django.db.backends.postgresql',
-'NAME': 'gabjonesgattass',
-'USER': 'postgres',
-'PASSWORD': 'G@b202025',
-'HOST': 'localhost',
-'PORT': '5432',
-}
-}
-
 #DATABASES = {
 #'default': {
-#'ENGINE': 'django.db.backends.sqlite3',
-#'NAME': BASE_DIR / 'db.sqlite3',
+#'ENGINE': 'django.db.backends.postgresql',
+#'NAME': 'gabjonesgattass',
+#'USER': 'postgres',
+#'PASSWORD': 'G@b202025',
+#'HOST': 'localhost',
+#'PORT': '5432',
 #}
 #}
+
+DATABASES = {
+'default': {
+'ENGINE': 'django.db.backends.sqlite3',
+'NAME': BASE_DIR / 'db.sqlite3',
+}
+}
 
 
 # Password validation
@@ -150,7 +150,7 @@ CACHES = {
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 # Default primary key field type
